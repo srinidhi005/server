5b70f375c888ea4c2807f616b3838e093d1af33d
srinidhi@rmiinsights.com <srinidhi@rmiinsights.com>
2019-10-21 18:24:16 +0000

    updated

--- seeders/20190306073705-users.js
+++ seeders/20190306073705-users.js
@@ -4,7 +4,10 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [
       { id: 1, username: 'user', password: 'user@rmi', role: 'user', email: 'rmi@example.com', created_at: new Date(), updated_at: new Date()},
-      { id: 2, username: 'admin', password: 'admin@rmi', role: 'admin', email: 'rmi@example.com', created_at: new Date(), updated_at: new Date()}
+      { id: 2, username: 'admin', password: 'admin@rmi', role: 'admin', email: 'rmi@example.com', created_at: new Date(), updated_at: new Date()},
+      { id: 3, username: 'varun', password: 'varun@123', role: 'owner', email: 'varun@rmiinsights.com', created_at: new Date(), updated_at: new Date()},
+      { id: 4, username: 'sylvester', password: 'sylvester@123', role: 'owner', email: 'varun@rmiinsights.com', created_at: new Date(), updated_at: new Date()}
+
     ], {});
   },
 
